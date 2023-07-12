5851881b211ec6d7dac0e966de920726265e7aee
Sariz Outsourcify <sariz@outsourcify.net>
2023-07-13 01:46:59 +0700

    feat: add proxy to vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
-// const { VITE_API_BASE_URL } = loadEnv('', process.cwd());
+const { VITE_API_BASE_URL } = loadEnv('', process.cwd());
 
 export default defineConfig({
   server: {
     port: 3000,
 
     proxy: {
-      '/api/v1': 'http://localhost:8080',
+      '/api/v1': VITE_API_BASE_URL,
     },
   },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
