3eb11ecd190dfefa7618284d4a2126dbecbc4bfe
Sariz Outsourcify <sariz@outsourcify.net>
2023-07-16 01:10:58 +0700

    fix: site env not show in sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,24 @@ import react from '@astrojs/react';
 import compress from 'astro-compress';
 import robotsTxt from 'astro-robots-txt';
 import sitemap from '@astrojs/sitemap';
+import { loadEnv } from 'vite';
 
-const site = import.meta.env.SITE_URL;
+const { PUBLIC_SITE_URL } = loadEnv(import.meta.env, process.cwd(), '');
+
+console.log('PUBLIC_SITE_URL', PUBLIC_SITE_URL);
 
 // https://astro.build/config
 export default defineConfig({
-  site: site,
+  site: PUBLIC_SITE_URL,
 
   integrations: [
+    // @astrojs/sitemap: https://docs.astro.build/en/guides/integrations-guide/sitemap/
+    sitemap(),
+    // astro-robots-txt: https://github.com/alextim/astro-lib/tree/main/packages/astro-robots-txt#readme
+    robotsTxt(),
+
     react(),
     // astro-compress: https://github.com/astro-community/astro-compress#readme
     compress(),
-    // astro-robots-txt: https://github.com/alextim/astro-lib/tree/main/packages/astro-robots-txt#readme
-    robotsTxt(),
-    // @astrojs/sitemap: https://docs.astro.build/en/guides/integrations-guide/sitemap/
-    sitemap(),
   ],
 });
