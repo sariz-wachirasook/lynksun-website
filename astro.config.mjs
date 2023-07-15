b2088f171cc16f8a70dc3422e4c445c2b1d286ae
Sariz Outsourcify <sariz@outsourcify.net>
2023-07-15 22:10:37 +0700

    perf: add sitemap, robot and compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import compress from 'astro-compress';
+import robotsTxt from 'astro-robots-txt';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    // astro-compress: https://github.com/astro-community/astro-compress#readme
+    compress(),
+    // astro-robots-txt: https://github.com/alextim/astro-lib/tree/main/packages/astro-robots-txt#readme
+    robotsTxt(),
+    // @astrojs/sitemap: https://docs.astro.build/en/guides/integrations-guide/sitemap/
+    sitemap(),
+  ],
 });
